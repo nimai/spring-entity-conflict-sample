10fe31121bc2bfd6ca24c71d7c52e07e543d6f7b
Nicolas Maître <nicolas.maitre@mcl-technologies.com>
2018-12-05 12:26:53 +0100

    Use h2 instead of real mysql db

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -42,9 +43,13 @@ public class DemoApplication {
     }
 
     @Bean(name = "myDataSource")
-    @ConfigurationProperties(prefix = "my.datasource")
     public DataSource datasource() {
-        return new org.apache.tomcat.jdbc.pool.DataSource();
+        return DataSourceBuilder.create()
+                .username("sa")
+                .password("")
+                .url("jdbc:h2:mem:testdb;DB_CLOSE_ON_EXIT=FALSE")
+                .driverClassName("org.h2.Driver")
+                .build();
     }
 
     @Bean(name = "myTm")
